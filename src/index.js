111285c14ad44298d4caf7365e8e4215cbfb9ded
Mafifiza <168578665+Mafifiza@users.noreply.github.com>
2025-07-01 02:26:26 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateVerse(event) {
+  event.preventDefault();
 
+  new Typewriter("#verse", {
+    strings: "The verse goes here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let verseFormElement = document.querySelector("#verse-generator-form");
+verseFormElement.addEventListener("submit", generateVerse);
